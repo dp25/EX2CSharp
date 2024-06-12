170daede7374c657307b3772c19adf17651e6173
dp25 <david.pardo60@gmail.com>
2024-06-12 14:10:31 +0300

    Created project

---
+++ B24_EX02/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B24_EX02
+{
+    internal class Player
+    {
+    }
+}
