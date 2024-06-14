92adbba6dbca1a4f987c09b5a34565fc020fdfd6
NimrodPardess <121054914+NimrodPardess@users.noreply.github.com>
2024-06-14 16:26:38 +0300

    class Player finished.

--- B24_EX02/Player.cs
+++ B24_EX02/Player.cs
@@ -8,5 +8,44 @@ namespace B24_EX02
 {
     internal class Player
     {
+        private readonly string r_PlayerName;
+        private readonly bool r_IsComputer;
+        private int m_PlayerPoints;
+
+        internal Player(string i_PlayerName, bool i_IsComputer)
+        {
+            r_PlayerName = i_PlayerName;
+            r_IsComputer = i_IsComputer;
+            m_PlayerPoints = 0;
+        }
+
+        internal string PlayerName
+        {
+            get
+            {
+                return r_PlayerName;
+            }
+        }
+
+        internal bool IsComputer
+        {
+            get
+            {
+                return r_IsComputer;
+            }
+        }
+
+        internal int PlayerPoints
+        {
+            get
+            {
+                return m_PlayerPoints;
+            }
+
+            set
+            {
+                m_PlayerPoints = value;
+            }
+        }
     }
 }
